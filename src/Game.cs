5897218bed631bada439237ed304cb32fb22a78e
normen <nokitz@t-online.de>
2026-04-28 23:19:31 +0200

    chore: initial Chickensoft scaffold

---
+++ src/Game.cs
@@ -0,0 +1,14 @@
+namespace SettlersX;
+
+using Godot;
+
+public partial class Game : Control
+{
+  public Button TestButton { get; private set; } = default!;
+  public int ButtonPresses { get; private set; }
+
+  public override void _Ready()
+    => TestButton = GetNode<Button>("%TestButton");
+
+  public void OnTestButtonPressed() => ButtonPresses++;
+}
